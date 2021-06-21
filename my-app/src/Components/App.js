37094b15c59138504c224ac5c4b1bce8b16719a4
Terry <incubus8@gmail.com>
2021-06-21 14:58:05 -0400

    initial starting point to contribute new code

---
+++ my-app/src/Components/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Logo from './Logo'
+import NavBar from './NavBar'
+import CoinContainer from './CoinContainer'
+
+ 
+
+function App(){
+
+    
+    return(
+        <div>
+          {/* <Logo/> */}
+          {/* <NavBar/> */}
+          <CoinContainer />
+        </div>
+)
+}
+
+
+export default App
---
+++ my-app/src/Components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo(){
+
+
+}
+
+
+export default Logo
